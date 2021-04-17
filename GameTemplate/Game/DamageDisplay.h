02f393c1ceb1ad7aa1b04ee035effc71083bef7e
nisikiori <mikan46494@gmail.com>
2021-04-17 16:48:44 +0900

    ダメージを跳ねるように表示させるようにした。

--- GameTemplate/Game/DamageDisplay.h
+++ GameTemplate/Game/DamageDisplay.h
@@ -13,6 +13,14 @@ public:
 	int m_deletetime = 10;
 	float m_Alpha = 1.0f;
 	prefab::CFontRender* m_fontRenderDamage = nullptr;
+
+
+	bool m_isxSpeedPositive;//ダメージのフォントが右に移動するか?(falseなら左に移動)
+	float m_xplusValue = 0;//フォントが表示される座標にプラスされるx座標の量
+	float m_xmoveSpeed;//フォントのx方向の移動速度
+
+	float m_yplusValue = 0;//フォントが表示される座標にプラスされるy座標の量
+	float m_ymoveSpeed = -10;//フォントのy方向の移動速度
 	
 	Vector3 m_damagePos;
 	Vector2 m_screenDamagePos;
