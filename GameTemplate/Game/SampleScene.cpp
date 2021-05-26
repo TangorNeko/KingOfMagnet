6eb8a3b3e1883122bac26fbad70bb7d86cacbe84
Kosei Kuze <(kbc20b19@stu.kawahara.ac.jp)>
2021-05-26 15:48:01 +0900

    hoge

--- GameTemplate/Game/SampleScene.cpp
+++ GameTemplate/Game/SampleScene.cpp
@@ -43,8 +43,10 @@ SampleScene::~SampleScene()
 
 	//DeleteGO(ssBGM);
 
-	//DeleteGO(m_onesPlaceSpriteRender);/*if (int(m_timeLimit) >= 10)
-		DeleteGO(m_tensPlaceSpriteRender);*/}
+	//DeleteGO(m_onesPlaceSpriteRender);
+	/*if (int(m_timeLimit) >= 10)
+		DeleteGO(m_tensPlaceSpriteRender);*/
+}
 
 bool SampleScene::Start()
 {
