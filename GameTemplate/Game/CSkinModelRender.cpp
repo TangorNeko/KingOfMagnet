ffd2f3551517cce0054120ff8517c64209d62bec
nisikiori <mikan46494@gmail.com>
2021-06-03 17:10:10 +0900

    CSkinModelRenderのリファクタリング

--- GameTemplate/Game/CSkinModelRender.cpp
+++ GameTemplate/Game/CSkinModelRender.cpp
@@ -3,30 +3,31 @@
 
 namespace prefab
 {
-	/// <summary>
-	/// モデルの初期化関数。アニメーションつき。
-	/// </summary>
-	/// <param name="modelPath">モデルファイルのパス(.tkm)</param>
-	/// <param name="skeletonPath">スケルトンファイルのパス(.tks)</param>
-	/// <param name="animationClips">アニメーションクリップの配列</param>
-	/// <param name="animationClipNum">アニメーションクリップの数</param>
 	void CSkinModelRender::Init(const char* modelPath, const char* skeletonPath, AnimationClip* animClips, int animClipNum)
 	{
+		//モデルの初期化データ
 		ModelInitData initData;
+
+		//影の初期化データ
 		ModelInitData shadowModelInitData;
 
+		//モデルのファイルパスの指定
 		initData.m_tkmFilePath = modelPath;
 		shadowModelInitData.m_tkmFilePath = modelPath;
 
+		//シェーダーパスの指定
 		initData.m_fxFilePath = "Assets/shader/shadowReceiver.fx";
 		shadowModelInitData.m_fxFilePath = "Assets/shader/shadow.fx";
 
+		//シェーダーの頂点シェーダーのエントリー関数名の指定
 		initData.m_vsEntryPointFunc = "VSMain";
 		shadowModelInitData.m_vsEntryPointFunc = "VSMain";
 
+		//シェーダーのピクセルシェーダーのエントリー関数名の指定
 		initData.m_vsSkinEntryPointFunc = "VSSkinMain";
 		shadowModelInitData.m_vsSkinEntryPointFunc = "VSSkinMain";
 
+		//スケルトンが存在している時はスケルトンを初期化
 		if (skeletonPath != nullptr)
 		{
 			m_skeleton.Init(skeletonPath);
@@ -34,12 +35,15 @@ namespace prefab
 			shadowModelInitData.m_skeleton = &m_skeleton;
 		}
 
+		//カラーバッファのフォーマットを指定
 		initData.m_colorBufferFormat = DXGI_FORMAT_R32G32B32A32_FLOAT;
 		shadowModelInitData.m_colorBufferFormat = DXGI_FORMAT_R32G32_FLOAT;
 
+		//モデルデータの上方向の軸を指定
 		initData.m_modelUpAxis = enModelUpAxisZ;
 		shadowModelInitData.m_modelUpAxis = enModelUpAxisZ;
 
+		//モデルに影を落とすために影のテクスチャを紐付ける
 		initData.m_expandShaderResoruceView = &PostEffectManager::GetInstance()->GetBlurShadowMap();
 
 		//定数バッファをモデルに紐付ける
@@ -51,14 +55,14 @@ namespace prefab
 		initData.m_expandConstantBufferSize[1] = CLightManager::GetInstance()->GetLigCameraDataSize();
 		initData.m_expandConstantBuffer[1] = CLightManager::GetInstance()->GetLigCameraDatas();
 
+		//モデルの初期化
 		m_model[eModel_View1].Init(initData);
 		m_model[eModel_View2].Init(initData);
 
-		//initData.m_fxFilePath = "Assets/shader/shadow.fx";
-		//initData.m_colorBufferFormat = DXGI_FORMAT_R32G32_FLOAT;
-
+		//影描画モデルの初期化
 		m_model[eModel_Shadow].Init(shadowModelInitData);
 
+		//アニメーション関連の初期化
 		m_animationClips = animClips;
 		m_animationClipNum = animClipNum;
 
@@ -68,36 +72,30 @@ namespace prefab
 		}
 	}
 
-	/// <summary>
-	/// モデルの初期化関数。
-	/// </summary>
-	/// <param name="modelPath">モデルファイルのパス(.tkm)</param>
-	/// <param name="skeletonPath">スケルトンファイルのパス(.tks)</param>
 	void CSkinModelRender::Init(const char* modelPath, const char* skeletonPath)
 	{
 		Init(modelPath, skeletonPath, nullptr, 0);
 	}
 
-	/// <summary>
-	/// モデルの初期化関数。モデルパスだけ版。
-	/// </summary>
-	/// <param name="modelPath">モデルファイルのパス(.tkm)</param>
 	void CSkinModelRender::Init(const char* modelPath)
 	{
 		Init(modelPath, nullptr, nullptr, 0);
 	}
 
-	//モデルの描画。
 	void CSkinModelRender::Render(RenderContext& rc, Camera* camera)
 	{
+		//レンダーコンテキストの描画先で分岐
 		switch (rc.GetRenderStep()) {
 		case RenderContext::eStep_RenderViewport1:
+			//画面1に描画
 			m_model[eModel_View1].Draw(rc, camera);
 			break;
 		case RenderContext::eStep_RenderViewport2:
+			//画面2に描画
 			m_model[eModel_View2].Draw(rc, camera);
 			break;
 		case RenderContext::eStep_RenderShadowMap:
+			//影を作るモデルの時だけ影を描画
 			if (m_isShadowCaster)
 			{
 				m_model[eModel_Shadow].Draw(rc, camera);
@@ -106,21 +104,15 @@ namespace prefab
 		}
 	}
 
-	/// <summary>
-	/// モデルの情報を更新する。
-	/// </summary>
 	void CSkinModelRender::UpdateModel()
 	{
+		//モデルをアップデート
 		for (auto& model : m_model) {
 			model.UpdateWorldMatrix(m_position, m_qRot, m_scale);
 			m_skeleton.Update(model.GetWorldMatrix());
 		}
 	}
 
-	/// <summary>
-	/// モデルの座標を設定する。
-	/// </summary>
-	/// <param name="pos">座標(Vector3)</param>
 	void CSkinModelRender::SetPosition(Vector3 pos)
 	{
 		m_position = pos;
@@ -128,10 +120,6 @@ namespace prefab
 		UpdateModel();
 	}
 
-	/// <summary>
-	/// モデルの回転を設定する。
-	/// </summary>
-	/// <param name="qRot">回転(Quaternion)</param>
 	void CSkinModelRender::SetRotation(Quaternion qRot)
 	{
 		m_qRot = qRot;
@@ -139,10 +127,6 @@ namespace prefab
 		UpdateModel();
 	}
 
-	/// <summary>
-	/// モデルの拡大率を設定する。
-	/// </summary>
-	/// <param name="scale">拡大率(Vector3)</param>
 	void CSkinModelRender::SetScale(Vector3 scale)
 	{
 		m_scale = scale;
@@ -150,31 +134,18 @@ namespace prefab
 		UpdateModel();
 	}
 
-	/// <summary>
-	/// モデルと線分が交差している?
-	/// </summary>
-	/// <param name="start">線分の始点</param>
-	/// <param name="end">線分の終点</param>
-	/// <param name="crossPoint">線分の交差点(戻り値、参照受け取り)</param>
-	/// <returns>交差しているか(している:true していない:false)</returns>
 	bool CSkinModelRender::isLineHitModel(const Vector3& start, const Vector3& end, Vector3& crossPoint)
 	{
 		//2画面用にCSkinModelRenderはModelクラスを2つ保持しているが、どちらもカメラ以外同じなので0番目のモデルで判定する。
 		return m_model[0].isLineHitModel(start, end, crossPoint);
 	}
 
-	/// <summary>
-	/// ボーンの名前からそのボーンのワールド行列を取得する。
-	/// </summary>
-	/// <param name="boneName">ボーンの名前</param>
-	/// <returns></returns>
 	Matrix CSkinModelRender::GetWorldMatrixFromBoneName(const wchar_t* boneName)
 	{
 		int boneNo = m_skeleton.FindBoneID(boneName);
 		return m_skeleton.GetBone(boneNo)->GetWorldMatrix();
 	}
 
-	//事前にモデルをロードしておく。
 	void CSkinModelRender::PreLoadModel(const char* tkmFilePath)
 	{
 		TkmFile* tkmFile = ResourceBankManager::GetInstance()->GetTkmFileFromBank(tkmFilePath);
--- GameTemplate/Game/CSkinModelRender.h
+++ GameTemplate/Game/CSkinModelRender.h
@@ -5,103 +5,96 @@ namespace prefab
 	class CSkinModelRender :public IGameObject
 	{
 	public:
+		//モデルの状態
 		enum EModel {
-			eModel_View1,
-			eModel_View2,
-			eModel_Shadow,
-			eModel_Num,
+			eModel_View1,//画面1に描画されるモデル
+			eModel_View2,//画面2に描画されるモデル
+			eModel_Shadow,//影を描画する用モデル
+			eModel_Num,//モデルの状態の数
 		};
 		//アニメーション速度
 		float m_animation_speed = 1.0;
 	private:
-		Skeleton m_skeleton; //スケルトン
-		Model m_model[eModel_Num]; //モデル
 
-		Vector3 m_position; //座標
-		Quaternion m_qRot; //回転
-		Vector3 m_scale; //拡大率
-
-		AnimationClip* m_animationClips;
-		int m_animationClipNum;
-		Animation m_animation;
-
-		bool m_isShadowCaster = false;
-
-		/// <summary>
-		/// モデルの情報を更新する。
-		/// </summary>
+		/**
+		 * @brief モデルの情報を更新する
+		*/
 		void UpdateModel();
 	public:
 		CSkinModelRender() :m_position(Vector3::Zero), m_qRot(g_quatIdentity), m_scale(Vector3::One) {}
 
+		/**
+		 * @brief モデルの描画
+		 * @param rc レンダーコンテキスト
+		 * @param camera 描画するカメラ
+		*/
 		void Render(RenderContext& rc,Camera* camera) override;
-
-		/// <summary>
-		/// モデルの初期化関数。アニメーションつき。
-		/// </summary>
-		/// <param name="modelPath">モデルファイルのパス(.tkm)</param>
-		/// <param name="skeletonPath">スケルトンファイルのパス(.tks)</param>
-		/// <param name="animationClips">アニメーションクリップの配列</param>
-		/// <param name="animationClipNum">アニメーションクリップの数</param>
+		
+		/**
+		 * @brief モデルの初期化関数　アニメーションつき
+		 * @param modelPath モデルファイルのパス(.tkm)
+		 * @param skeletonPath スケルトンファイルのパス(.tks)
+		 * @param animClips アニメーションクリップの配列
+		 * @param animClipNum アニメーションクリップの数
+		*/
 		void Init(const char* modelPath, const char* skeletonPath, AnimationClip* animClips, int animClipNum);
-
-		/// <summary>
-		/// モデルの初期化関数。
-		/// </summary>
-		/// <param name="modelPath">モデルファイルのパス(.tkm)</param>
-		/// <param name="skeletonPath">スケルトンファイルのパス(.tks)</param>
+		
+		/**
+		 * @brief モデルの初期化関数
+		 * @param modelPath モデルファイルのパス(.tkm)
+		 * @param skeletonPath スケルトンファイルのパス(.tks)
+		*/
 		void Init(const char* modelPath,const char* skeletonPath);
-
-		/// <summary>
-		/// モデルの初期化関数。モデルパスだけ版。
-		/// </summary>
-		/// <param name="modelPath">モデルファイルのパス(.tkm)</param>
+		
+		/**
+		 * @brief モデルの初期化関数　モデルパスだけ版
+		 * @param modelPath モデルファイルのパス(.tkm)
+		*/
 		void Init(const char* modelPath);
-
-		/// <summary>
-		/// アニメーションの再生
-		/// </summary>
-		/// <param name="animationNo">アニメーション番号</param>
-		/// <param name="interpolateTime">アニメーションの補完時間</param>
+		
+		/**
+		 * @brief アニメーションの再生
+		 * @param animationNo アニメーション番号
+		 * @param interpolateTime アニメーションの補完時間
+		*/
 		void PlayAnimation(int animationNo, float interpolateTime = 0.0f)
 		{
 			m_animation.Play(animationNo, interpolateTime);
 			m_animation.Progress(m_animation_speed /60.0f);
 			UpdateModel();
 		}
-
-		/// <summary>
-		/// アニメーションを再生しているか?
-		/// </summary>
-		/// <param name="pos"></param>
-		/// <returns>再生している:true 再生していない:false</returns>
+		
+		/**
+		 * @brief アニメーションを再生しているか?
+		 * @return 再生している:true 再生していない:false
+		*/
 		bool IsPlayingAnimation() const
 		{
 			return m_animation.IsPlaying();
 		}
-
-		/// <summary>
-		/// モデルの座標を設定する。
-		/// </summary>
-		/// <param name="pos">座標(Vector3)</param>
+		
+		/**
+		 * @brief モデルの座標を設定する
+		 * @param pos 座標
+		*/
 		void SetPosition(Vector3 pos);
-
-		/// <summary>
-		/// モデルの回転を設定する。
-		/// </summary>
-		/// <param name="qRot">回転(Quaternion)</param>
+		
+		/**
+		 * @brief モデルの回転を設定する
+		 * @param qRot 回転
+		*/
 		void SetRotation(Quaternion qRot);
-
-		/// <summary>
-		/// モデルの拡大率を設定する。
-		/// </summary>
-		/// <param name="scale">拡大率(Vector3)</param>
+		
+		/**
+		 * @brief モデルの拡大率を設定する
+		 * @param scale 拡大率
+		*/
 		void SetScale(Vector3 scale);
-
-		/// <summary>
-		/// モデルのワールド行列を直接セットする。
-		/// </summary>
-		/// <param name="world">ワールド行列(Matrix)</param>
+		
+		/**
+		 * @brief モデルのワールド行列を直接セットする
+		 * @param world ワールド行列
+		*/
 		void SetMatrix(Matrix world)
 		{
 			for (auto& model : m_model) {
@@ -110,75 +103,96 @@ namespace prefab
 			}
 		}
 
+		/**
+		 * @brief モデルが影を作るかどうかを設定する
+		 * @param isShadowCaster 作る:true 作らない:false
+		*/
 		void SetShadowCasterFlag(bool isShadowCaster)
 		{
 			m_isShadowCaster = isShadowCaster;
 		}
-
-		/// <summary>
-		/// アニメーションの再生フラグを取得
-		/// </summary>
-		/// <returns>再生している:true 再生していない:false</returns>
+		
+		/**
+		 * @brief アニメーションの再生フラグを取得
+		 * @return 再生している:true 再生していない:false
+		*/
 		bool GetAnimFlag()
 		{ 
 			return m_animation.IsPlaying(); 
 		}
-
-		/// <summary>
-		/// モデルと線分が交差している?
-		/// </summary>
-		/// <param name="start">線分の始点</param>
-		/// <param name="end">線分の終点</param>
-		/// <param name="crossPoint">線分の交差点(戻り値、参照受け取り)</param>
-		/// <returns>交差しているか(している:true していない:false)</returns>
+		
+		/**
+		 * @brief モデルと線分が交差している?
+		 * @param start 線分の始点
+		 * @param end 線分の終点
+		 * @param crossPoint 線分の交差点(戻り値、参照受け取り)
+		 * @return 交差している:true 交差していない:false
+		*/
 		bool isLineHitModel(const Vector3& start, const Vector3& end, Vector3& crossPoint);
-
-		/// <summary>
-		/// モデルの座標を取得
-		/// </summary>
-		/// <returns>座標(Vector3)</returns>
+		
+		/**
+		 * @brief モデルの座標を取得
+		 * @return 座標
+		*/
 		Vector3 GetPosition()
 		{
 			return m_position;
 		}
-
-		/// <summary>
-		/// モデルの回転を取得
-		/// </summary>
-		/// <returns>回転量(Quaternion)</returns>
+		
+		/**
+		 * @brief モデルの回転を取得
+		 * @return 回転
+		*/
 		Quaternion GetRotation()
 		{
 			return m_qRot;
 		}
-
-		/// <summary>
-		/// モデルの拡大率を取得
-		/// </summary>
-		/// <returns>拡大率(Vector3)</returns>
+		
+		/**
+		 * @brief モデルの拡大率を取得
+		 * @return 拡大率
+		*/
 		Vector3 GetScale()
 		{
 			return m_scale;
 		}
-
-		/// <summary>
-		/// モデルを取得
-		/// </summary>
-		/// <param name="model">ビューポート番号(カメラ番号)</param>
-		/// <returns>番号から取得したモデルへの参照</returns>
+		
+		/**
+		 * @brief モデルを取得
+		 * @param model ビューポート番号(カメラ番号)
+		 * @return 番号から取得したモデルへの参照
+		*/
 		Model& GetModel(EModel model = eModel_View1)
 		{
 			return m_model[model];
 		}
-
-		/// <summary>
-		/// ボーンの名前からそのボーンのワールド行列を取得する。
-		/// </summary>
-		/// <param name="boneName">ボーンの名前</param>
-		/// <returns></returns>
+		
+		/**
+		 * @brief ボーンの名前からそのボーンのワールド行列を取得する
+		 * @param boneName ボーンの名前
+		 * @return ボーンのワールド行列
+		*/
 		Matrix GetWorldMatrixFromBoneName(const wchar_t* boneName);
 
-		//事前にモデルをロードしておく。
+		/**
+		 * @brief 事前にモデルをロードしておく
+		 * @param tkmFilePath ロードしたいモデルのファイルパス
+		*/
 		static void PreLoadModel(const char* tkmFilePath);
+
+	private:
+		Skeleton m_skeleton; //スケルトン
+		Model m_model[eModel_Num]; //モデル
+
+		Vector3 m_position; //座標
+		Quaternion m_qRot; //回転
+		Vector3 m_scale; //拡大率
+
+		AnimationClip* m_animationClips = nullptr;//アニメーションクリップ
+		int m_animationClipNum = 0;//アニメーションクリップの数
+		Animation m_animation;//アニメーション。
+
+		bool m_isShadowCaster = false;//このモデルは影を作るか?
 	};
 }
 
