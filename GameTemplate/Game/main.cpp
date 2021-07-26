4aa4087d2c49c04c270e4f9702566728f3f04241
Jun_HomePC <mikan46494@gmail.com>
2021-07-26 21:35:18 +0900

    main.cppの定数コメント追加　関数分割

--- GameTemplate/Game/main.cpp
+++ GameTemplate/Game/main.cpp
@@ -10,14 +10,35 @@
 
 namespace
 {
-	const Vector3 LIGHTCAMERA_POSITION = { 500.0f,500.0f,-500.0f };
-	const Vector3 LIGHTCAMERA_TARGET = { 0.0f,0.0f,0.0f };
-	const Vector3 LIGHTCAMERA_UP = { 1.0f,0.0f,0.0f };
-	const float LIGHTCAMERA_WIDTH = 2000.0f;
-	const float LIGHTCAMERA_HEIGHT = 2000.0f;
+	const Vector3 LIGHTCAMERA_POSITION = { 500.0f,500.0f,-500.0f };	//ライトカメラの座標
+	const Vector3 LIGHTCAMERA_TARGET = { 0.0f,0.0f,0.0f };			//ライトカメラの注視点
+	const Vector3 LIGHTCAMERA_UP = { 1.0f,0.0f,0.0f };				//ライトカメラの上方向
+	const float LIGHTCAMERA_WIDTH = 2000.0f;						//ライトカメラが写す幅(平行光源なのでカメラは平行投影)
+	const float LIGHTCAMERA_HEIGHT = 2000.0f;						//ライトカメラが写す高さ
 }
 
+/**
+ * @brief モデルデータ等の事前ロード
+*/
 void PreLoad();
+
+/**
+ * @brief 各種マネージャーの初期化
+*/
+void InitManager();
+
+/**
+ * @brief ゲームループ
+ * @param renderContext レンダーコンテキスト 
+ * @param stopwatch FPS固定に使用するストップウォッチ
+*/
+void GameLoop(RenderContext& renderContext,Stopwatch& stopwatch);
+
+/**
+ * @brief 各種マネージャーの破棄
+*/
+void ReleaseManager();
+
 ///////////////////////////////////////////////////////////////////
 // ウィンドウプログラムのメイン関数。
 ///////////////////////////////////////////////////////////////////
@@ -26,41 +47,8 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 	//ゲームの初期化。
 	InitGame(hInstance, hPrevInstance, lpCmdLine, nCmdShow, TEXT("Game"));
 
-	//////////////////////////////////////
-	// ここから初期化を行うコードを記述する。
-	//////////////////////////////////////
-
-	//ゲームオブジェクトマネージャーのインスタンスを作成する。
-	GameObjectManager::CreateInstance();
-	PhysicsWorld::CreateInstance();
-
-	//2画面描画モードはOFFから始める。
-	GameObjectManager::GetInstance()->Set2ScreenMode(false);
-
-	//ライトマネージャーのインスタンスを作成
-	CLightManager::CreateInstance();
-	CLightManager::GetInstance()->SetLightCameraPosition(LIGHTCAMERA_POSITION);
-	CLightManager::GetInstance()->SetLightCameraTarget(LIGHTCAMERA_TARGET);
-	CLightManager::GetInstance()->SetLightCameraUp(LIGHTCAMERA_UP);
-	CLightManager::GetInstance()->SetLightCameraUpdateProjMatrixFunc(Camera::enUpdateProjMatrixFunc_Ortho);
-	CLightManager::GetInstance()->SetLightCameraWidth(LIGHTCAMERA_WIDTH);
-	CLightManager::GetInstance()->SetLightCameraHeight(LIGHTCAMERA_HEIGHT);
-
-	//リソースマネージャのインスタンスを作成
-	ResourceBankManager::CreateInstance();
-
-	CSoundEngine::CreateInstance();
-	CSoundEngine::GetInstance()->Init();
-
-	EffectEngine::CreateInstance();
-
-	PostEffectManager::CreateInstance();
-	//ブルームフラグ、シャドウフラグの順番
-	PostEffectManager::GetInstance()->Init(true,true);
-	//////////////////////////////////////////////////
-	
-	//FPS固定用ストップウォッチ
-	Stopwatch stopwatch;
+	//マネージャーの初期化
+	InitManager();
 
 	//モデルデータ、エフェクトデータの事前ロード。
 	PreLoad();
@@ -68,77 +56,36 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 	//ゲーム設定の読み込み
 	GameOption::ReadOption();
 
-	TransitionGenerator* tg = NewGO<TransitionGenerator>(5, "TransitionGenerator");
+	//トランジションジェネレーターを作成
+	NewGO<TransitionGenerator>(5, "TransitionGenerator");
 
 	//トランジション
 	TransitionGenerator::GetInstance()->TransitionInit(TransitionGenerator::TransitionName::Circle, TRANSITION_TIME_NORMAL, false);
 
 	//タイトルシーンを作成。
-	TitleScene* title = NewGO<TitleScene>(6, "titlescene");
+	NewGO<TitleScene>(6, "titlescene");
 
 	//////////////////////////////////////
 	// 初期化を行うコードを書くのはここまで！！！
 	//////////////////////////////////////
 	auto& renderContext = g_graphicsEngine->GetRenderContext();
 
+	//FPS固定用ストップウォッチ
+	Stopwatch stopwatch;
+
 	// ここからゲームループ。
 	while (DispatchWindowMessage())
 	{
-		//FPS計測用ストップウォッチの計測開始
-		stopwatch.Start();
-
-		//レンダリング開始。
-		g_engine->BeginFrame();
-
-		//////////////////////////////////////
-		//ここから絵を描くコードを記述する。
-		//////////////////////////////////////
-
-		GameObjectManager::GetInstance()->ExecuteUpdate();
-
-		//Renderはモデル等、エフェクトを受けるものを描画する
-		GameObjectManager::GetInstance()->ExecuteRender(renderContext);
-
-		//PostRenderはスプライト、フォント等、エフェクトを受けないものを描画する
-		GameObjectManager::GetInstance()->ExecutePostRender(renderContext);
-
-		//////////////////////////////////////
-		//絵を描くコードを書くのはここまで！！！
-		//////////////////////////////////////
-		g_engine->EndFrame();
-
-		//スピンロックを行う。
-		int restTime = 0;
-		do {
-			stopwatch.Stop();
-			restTime = 16 - (int)stopwatch.GetElapsedMillisecond();
-		} while (restTime > 0);
-
-
-		//ストップウォッチの計測終了
-		stopwatch.Stop();
-		//デルタタイムをストップウォッチの計測時間から、計算する
-		GameTime::GetInstance().PushFrameDeltaTime((float)stopwatch.GetElapsed());
+		GameLoop(renderContext, stopwatch);
 	}
-	//ゲームオブジェクトマネージャーを削除。
-	GameObjectManager::DeleteInstance();
 
-	//ライトマネージャーを削除
-	CLightManager::DeleteInstance();
-	
-	//tkmFileManagerを削除
-	ResourceBankManager::DeleteInstance();
+	//マネージャーの破棄
+	ReleaseManager();
 
-	//サウンドエンジンを削除
-	CSoundEngine::DeleteInstance();
-	
-	//ポストエフェクトマネージャーを削除
-	PostEffectManager::DeleteInstance();
 	return 0;
 }
 
 //使用するモデル、エフェクトの事前ロード、
-//TODO:後からcsvファイル等にまとめて連想配列などで使用できるようにしたいが...
 void PreLoad()
 {
 	//モデルの事前ロード
@@ -176,4 +123,92 @@ void PreLoad()
 	prefab::CEffect::PreLoadEffect(u"Assets/effect/斥力弾発射.efk");
 	prefab::CEffect::PreLoadEffect(u"Assets/effect/引力バースト.efk");
 	prefab::CEffect::PreLoadEffect(u"Assets/effect/斥力バースト.efk");
+}
+
+void InitManager()
+{
+	//ゲームオブジェクトマネージャーのインスタンスを作成する。
+	GameObjectManager::CreateInstance();
+	PhysicsWorld::CreateInstance();
+
+	//2画面描画モードはOFFから始める。
+	GameObjectManager::GetInstance()->Set2ScreenMode(false);
+
+	//ライトマネージャーのインスタンスを作成
+	CLightManager::CreateInstance();
+	CLightManager::GetInstance()->SetLightCameraPosition(LIGHTCAMERA_POSITION);
+	CLightManager::GetInstance()->SetLightCameraTarget(LIGHTCAMERA_TARGET);
+	CLightManager::GetInstance()->SetLightCameraUp(LIGHTCAMERA_UP);
+	CLightManager::GetInstance()->SetLightCameraUpdateProjMatrixFunc(Camera::enUpdateProjMatrixFunc_Ortho);
+	CLightManager::GetInstance()->SetLightCameraWidth(LIGHTCAMERA_WIDTH);
+	CLightManager::GetInstance()->SetLightCameraHeight(LIGHTCAMERA_HEIGHT);
+
+	//リソースマネージャのインスタンスを作成
+	ResourceBankManager::CreateInstance();
+
+	CSoundEngine::CreateInstance();
+	CSoundEngine::GetInstance()->Init();
+
+	EffectEngine::CreateInstance();
+
+	PostEffectManager::CreateInstance();
+	//ブルームフラグ、シャドウフラグの順番
+	PostEffectManager::GetInstance()->Init(true, true);
+}
+
+void GameLoop(RenderContext& renderContext, Stopwatch& stopwatch)
+{
+	//FPS計測用ストップウォッチの計測開始
+	stopwatch.Start();
+
+	//レンダリング開始。
+	g_engine->BeginFrame();
+
+	//////////////////////////////////////
+	//ここから絵を描くコードを記述する。
+	//////////////////////////////////////
+
+	GameObjectManager::GetInstance()->ExecuteUpdate();
+
+	//Renderはモデル等、エフェクトを受けるものを描画する
+	GameObjectManager::GetInstance()->ExecuteRender(renderContext);
+
+	//PostRenderはスプライト、フォント等、エフェクトを受けないものを描画する
+	GameObjectManager::GetInstance()->ExecutePostRender(renderContext);
+
+	//////////////////////////////////////
+	//絵を描くコードを書くのはここまで！！！
+	//////////////////////////////////////
+	g_engine->EndFrame();
+
+	//スピンロックを行う。
+	int restTime = 0;
+	do {
+		stopwatch.Stop();
+		restTime = 16 - (int)stopwatch.GetElapsedMillisecond();
+	} while (restTime > 0);
+
+
+	//ストップウォッチの計測終了
+	stopwatch.Stop();
+	//デルタタイムをストップウォッチの計測時間から、計算する
+	GameTime::GetInstance().PushFrameDeltaTime((float)stopwatch.GetElapsed());
+}
+
+void ReleaseManager()
+{
+	//ゲームオブジェクトマネージャーを削除。
+	GameObjectManager::DeleteInstance();
+
+	//ライトマネージャーを削除
+	CLightManager::DeleteInstance();
+
+	//tkmFileManagerを削除
+	ResourceBankManager::DeleteInstance();
+
+	//サウンドエンジンを削除
+	CSoundEngine::DeleteInstance();
+
+	//ポストエフェクトマネージャーを削除
+	PostEffectManager::DeleteInstance();
 }
